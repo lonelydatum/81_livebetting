4d9e99f4948a2f33d946096ede5ddba4c52d989e
Gar <gar@lonelydatum.com>
2024-09-24 15:35:11 -0400

    on

---
+++ dev/hockey_160x600/_js/main.js
@@ -0,0 +1,22 @@
+ 
+
+
+import {init, start, colors} from '../../_common/js/hockey.js'
+
+
+const barOptions  ={
+	TOTAL: 12,
+	WIDTH: 132,
+	HEIGHT: 15,
+	GAP: 10,
+	hero: .9,
+	scale: {scale:.66, x:-69, y:-114} 
+}
+
+start(barOptions)
+	
+
+
+
+module.exports = {};
+
---
+++ dev/hockey_300x250/_js/main.js
@@ -0,0 +1,26 @@
+
+
+import {init, start, colors} from '../../_common/js/hockey.js'
+
+
+
+
+const barOptions  ={
+	TOTAL: 12,
+	WIDTH: 278,
+	HEIGHT: 4,
+	GAP: 20,
+	hero: 0.9,
+	scale: {scale:.8, x:-196, y:-49} 
+	
+}
+
+
+start(barOptions)
+
+
+
+
+
+module.exports = {};
+
